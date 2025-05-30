d71c3ad41f6eb5e1e0b60d3aa4319ad13889e600
Bruno <User@DESKTOP-HF4OIU3>
2025-05-30 09:46:03 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite a nota do primeiro semestre");
+            double P = double.Parse(Console.ReadLine()!,CultureInfo.InvariantCulture!);
+            Console.WriteLine("Digite a nota do segundo semestre");
+            double M = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture!);
+            double notaFinal = P + M;
+
+            Console.WriteLine("O Seu resultado final foi: " + notaFinal.ToString("F1",CultureInfo.InvariantCulture) + " pontos");   
+            
+            if (notaFinal >= 60) {
+                Console.WriteLine("Você foi aprovado!");
+            } else {
+                Console.WriteLine("voce foi reprovado!"); 
+            }
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
